e43a5f6b34d4e8049480d7c7e7fe975abe612fdf
Williammer <supervegeter@gmail.com>
2015-07-20 22:09:52 +0800

    get start source.

---
+++ hapijs-demo/get-started/index.js
@@ -0,0 +1,42 @@
+var Hapi = require('hapi');
+var Good = require('good');
+
+var server = new Hapi.Server();
+server.connection({ port: 3000 });
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply('Hello, world!');
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/{name}',
+    handler: function (request, reply) {
+        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+    }
+});
+
+server.register({
+    register: Good,
+    options: {
+        reporters: [{
+            reporter: require('good-console'),
+            events: {
+                response: '*',
+                log: '*'
+            }
+        }]
+    }
+}, function (err) {
+    if (err) {
+        throw err; // something bad happened loading the plugin
+    }
+
+    server.start(function () {
+        server.log('info', 'Server running at: ' + server.info.uri);
+    });
+});
