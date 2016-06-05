0bd82118ccfcb3f0e6f2e6ad69c3aaa144090e38
Williammer <supervegeter@gmail.com>
2016-06-05 22:24:13 +0800

    Added es6 composer fn.

--- es6-try/es6-try.js
+++ es6-try/es6-try.js
@@ -17,4 +17,20 @@ let show = (info, ...others) => { // Spread/Rest turn arguments into real array
     }
 };
 
-show("I love u.", "and you", "and youAll");
+// show("I love u.", "and you", "and youAll");
+
+function chained(functions) {
+  return arg => functions.reduce((prev, cur) => cur(prev), arg);
+}
+
+function f1(x){ return x*2 }
+function f2(x){ return x+2 }
+function f3(x){ return Math.pow(x,2) }
+
+function f4(x){ return x.split("").concat().reverse().join("").split(" ")}
+function f5(xs){ return xs.concat().reverse() }
+function f6(xs){ return xs.join("_") }
+
+print(chained([f1,f2,f3])(0))
+print(chained([f1,f2,f3])(2))
+print(chained([f3,f2,f1])(2))
