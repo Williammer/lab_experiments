b526cb0d9cadef70f6fae145e2e322a3489693e0
Williammer <supervegeter@gmail.com>
2016-06-05 22:29:32 +0800

    refactor to better es6 fn

--- es6-try/es6-try.js
+++ es6-try/es6-try.js
@@ -1,36 +1,34 @@
-let print = (input) => console.log(input);
-let upper = (input) => input.toUpperCase();
+const print = input => console.log(input)
+const upper = input => input.toUpperCase()
 
-let show = (info, ...others) => { // Spread/Rest turn arguments into real array
-    print(`param: ${info}`);
+const show = (info, ...others) => { // Spread/Rest turn arguments into real array
+    print(`param: ${info}`)
 
     if (!(others.length > 0)) {
-        print(`no other param, others.length: ${others.length}`);
-        return;
+        print(`no other param, others.length: ${others.length}`)
+        return
     }
 
-    // for(let param of others) { // 1. for...of loop
+    // for(const param of others) { // 1. for...of loop
     for (var ret, param, iterator = others[Symbol.iterator]();
         (ret = iterator.next()) && !ret.done; /* not other action for each loop */ ) { // 2. Symbol.iterator loop
-        param = ret.value;
-        print(`other param: ${upper(param)}`); // template literals
+        param = ret.value
+        print(`other param: ${upper(param)}`) // template literals
     }
-};
-
-// show("I love u.", "and you", "and youAll");
-
-function chained(functions) {
-  return arg => functions.reduce((prev, cur) => cur(prev), arg);
 }
 
-function f1(x){ return x*2 }
-function f2(x){ return x+2 }
-function f3(x){ return Math.pow(x,2) }
+show("I love u.", "and you", "and youAll")
 
-function f4(x){ return x.split("").concat().reverse().join("").split(" ")}
-function f5(xs){ return xs.concat().reverse() }
-function f6(xs){ return xs.join("_") }
+const compose = functions => arg => functions.reduce((composed, f) => f(composed), arg)
 
-print(chained([f1,f2,f3])(0))
-print(chained([f1,f2,f3])(2))
-print(chained([f3,f2,f1])(2))
+const f1 = (x) => x*2
+const f2 = (x) => x+2
+const f3 = (x) => Math.pow(x,2)
+
+const f4 = (x) => x.split("").concat().reverse().join("").split(" ")
+const f5 = (xs) => xs.concat().reverse()
+const f6 = (xs) => xs.join("_")
+
+print(compose([f1,f2,f3])(0))
+print(compose([f1,f2,f3])(2))
+print(compose([f3,f2,f1])(2))
